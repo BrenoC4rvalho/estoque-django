71dd27c15bd82028c35249484e6dc88c37ab97b4
BrenoC4rvalho <bc101004@gmail.com>
2024-11-29 20:46:39 -0300

    arrumei bugs

--- conf/settings.py
+++ conf/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'estoque.urls'
+ROOT_URLCONF = 'conf.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'estoque.wsgi.application'
+WSGI_APPLICATION = 'conf.wsgi.application'
 
 
 # Database
